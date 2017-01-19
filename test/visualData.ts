fd8ff09f3ad26e77ce5ab3fbea6474f5ae3bc2b6
Ignat Vilesov <v-viig@microsoft.com>
2017-01-19 17:06:57 +0300

    14190 “Timeline: Migrate the existing unit tests from VSO to GitHub repo”

---
+++ test/visualData.ts
@@ -0,0 +1,81 @@
+/*
+ *  Power BI Visualizations
+ *
+ *  Copyright (c) Microsoft Corporation
+ *  All rights reserved.
+ *  MIT License
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the ""Software""), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+/// <reference path="_references.ts"/>
+
+module powerbi.extensibility.visual.test {
+    // powerbi.extensibility.visual.test
+    import getDateRange = powerbi.extensibility.visual.test.helpers.getDateRange;
+
+    // powerbi.extensibility.utils.type
+    import ValueType = powerbi.extensibility.utils.type.ValueType;
+
+    // powerbi.extensibility.utils.test
+    import TestDataViewBuilder = powerbi.extensibility.utils.test.dataViewBuilder.TestDataViewBuilder;
+
+    export class TimelineData extends TestDataViewBuilder {
+        public static ColumnCategory: string = "Date";
+
+        public valuesCategory: Date[] = getDateRange(
+            new Date(2016, 0, 1),
+            new Date(2016, 0, 10),
+            1000 * 24 * 3600);
+
+        public getDataView(columnNames?: string[]): powerbi.DataView {
+            return this.createCategoricalDataViewBuilder([
+                {
+                    source: {
+                        displayName: TimelineData.ColumnCategory,
+                        roles: { Category: true },
+                        type: ValueType.fromDescriptor({ dateTime: true })
+                    },
+                    values: this.valuesCategory
+                }
+
+            ], null, columnNames).build();
+        }
+
+        public getUnWorkableDataView(columnNames?: string[]): powerbi.DataView {
+            return this.createCategoricalDataViewBuilder([
+                {
+                    source: {
+                        displayName: "Country",
+                        type: ValueType.fromDescriptor({ text: true }),
+                        roles: { Category: true }
+                    },
+                    values: [
+                        "Australia",
+                        "Canada",
+                        "France",
+                        "Germany",
+                        "United Kingdom",
+                        "United States"
+                    ]
+                }
+            ], null, null).build();
+        }
+    }
+}
