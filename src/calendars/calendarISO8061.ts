1b301bf4fc4e0b405afa45ee6b9f1ea604a4ea34
Evgenii Elkin <v-evelk@microsoft.com>
2021-02-09 16:20:39 +0300

    Extended comments + some refactoring after review

--- src/calendars/calendarISO8061.ts
+++ src/calendars/calendarISO8061.ts
@@ -22,30 +22,41 @@ export class CalendarISO8061 extends Calendar {
 
     public determineWeek(date: Date): number[] {
         const year: number = this.determineWeekYear(date);
-        const dateOfFirstWeek: Date = this.getDateOfFirstWeek(year);
         const dateOfFirstFullWeek: Date = this.getDateOfFirstFullWeek(year);
-
         const weeks: number = Utils.GET_NUMBER_OF_WEEKS_BETWEEN_DATES(dateOfFirstFullWeek, date);
 
         return [weeks, year];
     }
 
+    /*
+    Returns a correct year for passed as a parameter date.
+    Regarding ISO, first week can start from 29th December to 4th January so,
+    If we pass date as 2021-01-03 for US calendar it belongs to the first week and should return 2021
+    However, for ISO it still belongs to the latest week of 2020 year and here we have to return 2020
+    */
     private determineWeekYear(date: Date): number {
         let dateYear = date.getFullYear();
         const dateOfFirstWeek: Date = this.getDateOfFirstWeek(dateYear);
         const dateOfFirstWeekNext: Date = this.getDateOfFirstWeek(dateYear + 1);
-        const dateOfFirstWeekPrevious: Date = this.getDateOfFirstWeek(dateYear - 1);
 
-        if (dateYear === 2019 && date.getDate() === 31 && date.getMonth() === 11) {
-            debugger;
-        }
-        
         if (date < dateOfFirstWeek) {
-            dateYear = new Date((dateOfFirstWeekPrevious.getTime() + dateOfFirstWeek.getTime()) / 2).getFullYear();
+            // This scenario works when the first date of ISO week year starts from the beginning of January: 1th, 2nd, 3rd, 4th
+            // 
+            // Input date:                                   January 3, 2021 [left condition expression]
+            // The first date of 2021 regarding ISO weeks:   January 4, 2021 [right condition expression]
+            // Returning ISO week year for January 3, 2021:  2020
+            //
+            // To get the ISO week year correctly, the algorythm just deduct 1 from actual year that is 2021 to get 2020
+            dateYear -= 1
         } else if (date >= dateOfFirstWeekNext) {
-            dateYear = dateYear + 1;
-        } else {
-            dateYear = new Date((dateOfFirstWeek.getTime() + dateOfFirstWeekNext.getTime()) / 2).getFullYear();
+            // This scenario works when the first date of ISO week year starts from the last days of actual previous year: December, 29th | 30th | 31st
+            //
+            // Input date:                                 December 31, 2019 [left condition expression]
+            // The first date of 2020 regarding ISO weeks: December 30, 2019 [right condition expression]
+            // Returning ISO week year:                    2020
+            //
+            // To get the ISO week year correctly, the algorythm just add 1 to actual year that is 2019 to get 2020
+            dateYear += 1;
         }
 
         return dateYear;
@@ -53,13 +64,26 @@ export class CalendarISO8061 extends Calendar {
 
     public getDateOfFirstWeek(year: number): Date {
         const dateOfFirstJan = new Date(year, 0, 1);
-        const dayOfWeek = dateOfFirstJan.getDay();
+        const dayOfFirstJanWeek = dateOfFirstJan.getDay();
+        const firstJanDig = 1;
 
         let dateOfFirstWeek = dateOfFirstJan;
-        if (dayOfWeek <= 4) {
-            dateOfFirstWeek.setDate(dateOfFirstJan.getDate() - dateOfFirstJan.getDay() + 1);
+        // The first week regarding ISO has to contain Thursday (4th day in the week)
+        if (dayOfFirstJanWeek <= 4) {
+            // If 1st January is Monday, Tuesday, Wednesday or Thursday => the first week date should be adjusted to left up to Monday
+            // 1st Jan is Tuesday    setDate(1 - 2 + 1) = setDate(0)  => 31st December (last day of previous month)
+            // 1st Jan is Monday     setDate(1 - 1 + 1) = setDate(1)  => 1st  January  (nothing has changed)
+            // 1st Jan is Wednesday  setDate(1 - 3 + 1) = setDate(-1) => 30th December
+            // 1st Jan is Thursday   setDate(1 - 4 + 1) = setDate(-2) => 29th December
+            // Digit 1 here is just  constant to correct calculation
+            dateOfFirstWeek.setDate(firstJanDig - dayOfFirstJanWeek + 1);
         } else {
-            dateOfFirstWeek.setDate(dateOfFirstJan.getDate() + 8 - dateOfFirstJan.getDay());
+            // If 1st January is Friday, Saturday or Sunday => the first week date should be adjusted to right up to Monday
+            // 1st Jan is Friday     setDate(1 + 8 - 5) = setDate(4) => 4th January
+            // 1st Jan is Saturday   setDate(1 + 8 - 6) = setDate(3) => 3rd January
+            // 1st Jan is Sunday     setDate(1 + 8 - 7) = setDate(2) => 2nd January
+            // Digit 8 here is just constant to correct calculation that represents a week + 1
+            dateOfFirstWeek.setDate(firstJanDig - dayOfFirstJanWeek + 8);
         }
 
         if (!this.dateOfFirstWeek[year]) {
